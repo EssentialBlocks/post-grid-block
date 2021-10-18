199c25c85d7a93f79ed38b4846053f53ba0b8b9f
Jamil Uddin <jamilbd07@gmail.com>
2021-10-18 16:54:10 +0600

    fix query issue

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-polyfill'), 'version' => 'f52dd3f89b85179abe88c60c076ddacf');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-polyfill'), 'version' => '57626890c87ffbaa4642847ba72e7b84');
